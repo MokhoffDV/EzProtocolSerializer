c7fdf06f929d7b9af0242d08457f5cdfabd5a9c9
Danila Mokhov <danila.mokhov@graebert.com>
2024-05-22 10:00:29 +0200

    Reorganizing tests into more distinct categories

--- tests/ez_protocol_serializer_tests.cpp
+++ tests/ez_protocol_serializer_tests.cpp
@@ -4,46 +4,80 @@
 using ez::protocol_serializer;
 using buffer_source = ez::protocol_serializer::buffer_source;
 using result_code = ez::protocol_serializer::result_code;
-TEST(Constructing, Constructors) {
-    // Default
-    protocol_serializer psDefault;
-    EXPECT_EQ(psDefault.get_is_little_endian(), false);
-    EXPECT_EQ(psDefault.get_buffer_source(), protocol_serializer::buffer_source::internal);
-    EXPECT_EQ(psDefault.get_internal_buffer().get(), psDefault.get_working_buffer());
-    EXPECT_EQ(psDefault.get_internal_buffer_length(), 0);
-    EXPECT_EQ(psDefault.get_fields_list().size(), 0);
+TEST(Constructing, DefaultConstructor)
+{
+    protocol_serializer ps;
+    EXPECT_EQ(ps.get_is_little_endian(), false);
+    EXPECT_EQ(ps.get_buffer_source(), protocol_serializer::buffer_source::internal);
+    EXPECT_EQ(ps.get_internal_buffer().get(), ps.get_working_buffer());
+    EXPECT_EQ(ps.get_internal_buffer_length(), 0);
+    EXPECT_EQ(ps.get_fields_list().size(), 0);
+}
 
-    // Constructor 1
+TEST(Constructing, Constructor1)
+{
     unsigned char externalBuffer[32];
-    protocol_serializer psConstructor1(true, buffer_source::external, externalBuffer);
-    EXPECT_EQ(psConstructor1.get_is_little_endian(), true);
-    EXPECT_EQ(psConstructor1.get_buffer_source(), protocol_serializer::buffer_source::external);
-    EXPECT_NE(psConstructor1.get_internal_buffer().get(), psConstructor1.get_working_buffer());
-    EXPECT_EQ(psConstructor1.get_external_buffer(), psConstructor1.get_working_buffer());
-    EXPECT_EQ(psConstructor1.get_internal_buffer_length(), 0);
-    EXPECT_EQ(psConstructor1.get_fields_list().size(), 0);
-
-    // Constructor 2
-    protocol_serializer psConstructor2({
+    memset(externalBuffer, 1, 32);
+    protocol_serializer ps({
                                 {"field_1", 8},
                                 {"field_2", 8},
                                 {"field_3", 3},
-                            }, true, buffer_source::internal, externalBuffer);
-    EXPECT_EQ(psConstructor2.get_is_little_endian(), true);
-    EXPECT_EQ(psConstructor2.get_buffer_source(), protocol_serializer::buffer_source::internal);
-    EXPECT_NE(psConstructor2.get_internal_buffer().get(), nullptr);
-    EXPECT_EQ(psConstructor2.get_external_buffer(), nullptr);
-    EXPECT_EQ(psConstructor2.get_internal_buffer_length(), 3);
-    EXPECT_EQ(psConstructor2.get_fields_list().size(), 3);
+                           }, true, buffer_source::external, externalBuffer);
+    EXPECT_EQ(ps.get_is_little_endian(), true);
+    EXPECT_EQ(ps.get_buffer_source(), protocol_serializer::buffer_source::external);
+    EXPECT_NE(ps.get_internal_buffer().get(), nullptr);
+    EXPECT_EQ(ps.get_external_buffer(), externalBuffer);
+    EXPECT_EQ(ps.get_working_buffer(), ps.get_external_buffer());
+    EXPECT_EQ(ps.get_internal_buffer_length(), 3);
+    EXPECT_EQ(ps.get_fields_list().size(), 3);
+
+    const int intialField2Value = ps.read<int>("field_2");
+    memset(externalBuffer, 2, 32);
+    const int newField2Value = ps.read<int>("field_2");
+    EXPECT_NE(intialField2Value, newField2Value);
+    ps.append_field({"field_4", 5}, true);
+    const int newerField2Value = ps.read<int>("field_2");
+    EXPECT_EQ(newField2Value, newerField2Value);
+}
+
+TEST(Constructing, Constructor2)
+{
+    protocol_serializer ps({
+                                {"field_1", 8},
+                                {"field_2", 8},
+                                {"field_3", 3},
+                           }, true, buffer_source::internal);
+    EXPECT_EQ(ps.get_is_little_endian(), true);
+    EXPECT_EQ(ps.get_buffer_source(), protocol_serializer::buffer_source::internal);
+    EXPECT_NE(ps.get_internal_buffer().get(), nullptr);
+    EXPECT_EQ(ps.get_external_buffer(), nullptr);
+    EXPECT_EQ(ps.get_internal_buffer_length(), 3);
+    EXPECT_EQ(ps.get_fields_list().size(), 3);
+}
+
+TEST(Constructing, Copy)
+{
+    protocol_serializer ps({
+                                {"field_1", 8},
+                                {"field_2", 8},
+                                {"field_3", 3},
+                           }, true, buffer_source::internal);
+    EXPECT_EQ(ps.get_is_little_endian(), true);
+    EXPECT_EQ(ps.get_buffer_source(), protocol_serializer::buffer_source::internal);
+    EXPECT_NE(ps.get_internal_buffer().get(), nullptr);
+    EXPECT_EQ(ps.get_external_buffer(), nullptr);
+    EXPECT_EQ(ps.get_working_buffer(), ps.get_internal_buffer().get());
+    EXPECT_EQ(ps.get_internal_buffer_length(), 3);
+    EXPECT_EQ(ps.get_fields_list().size(), 3);
 
     // Copy constructor
-    protocol_serializer psCopyConstructor(psConstructor2);
-    EXPECT_EQ(psCopyConstructor.get_is_little_endian(), psConstructor2.get_is_little_endian());
-    EXPECT_EQ(psCopyConstructor.get_buffer_source(), psConstructor2.get_buffer_source());
-    EXPECT_NE(psCopyConstructor.get_internal_buffer().get(), psConstructor2.get_internal_buffer().get());
-    EXPECT_EQ(psCopyConstructor.get_external_buffer(), psConstructor2.get_external_buffer());
-    EXPECT_EQ(psCopyConstructor.get_internal_buffer_length(), psConstructor2.get_internal_buffer_length());
-    EXPECT_EQ(psCopyConstructor.get_fields_list().size(), psConstructor2.get_fields_list().size());
+    protocol_serializer psCopyConstructor(ps);
+    EXPECT_EQ(psCopyConstructor.get_is_little_endian(), ps.get_is_little_endian());
+    EXPECT_EQ(psCopyConstructor.get_buffer_source(), ps.get_buffer_source());
+    EXPECT_NE(psCopyConstructor.get_internal_buffer().get(), ps.get_internal_buffer().get());
+    EXPECT_EQ(psCopyConstructor.get_external_buffer(), ps.get_external_buffer());
+    EXPECT_EQ(psCopyConstructor.get_internal_buffer_length(), ps.get_internal_buffer_length());
+    EXPECT_EQ(psCopyConstructor.get_fields_list().size(), ps.get_fields_list().size());
 
     // Copy assignment operator
     protocol_serializer psCopyAssignment(psCopyConstructor);
@@ -53,9 +87,24 @@ TEST(Constructing, Constructors) {
     EXPECT_EQ(psCopyAssignment.get_external_buffer(), psCopyConstructor.get_external_buffer());
     EXPECT_EQ(psCopyAssignment.get_internal_buffer_length(), psCopyConstructor.get_internal_buffer_length());
     EXPECT_EQ(psCopyAssignment.get_fields_list().size(), psCopyConstructor.get_fields_list().size());
+}
+
+TEST(Constructing, Move)
+{
+    protocol_serializer ps({
+                                {"field_1", 8},
+                                {"field_2", 8},
+                                {"field_3", 3},
+                           }, true, buffer_source::internal);
+    EXPECT_EQ(ps.get_is_little_endian(), true);
+    EXPECT_EQ(ps.get_buffer_source(), protocol_serializer::buffer_source::internal);
+    EXPECT_NE(ps.get_internal_buffer().get(), nullptr);
+    EXPECT_EQ(ps.get_external_buffer(), nullptr);
+    EXPECT_EQ(ps.get_internal_buffer_length(), 3);
+    EXPECT_EQ(ps.get_fields_list().size(), 3);
 
     // Move constructor
-    protocol_serializer psMoveConstructor(std::move(psConstructor2));
+    protocol_serializer psMoveConstructor(std::move(ps));
     EXPECT_EQ(psMoveConstructor.get_is_little_endian(), true);
     EXPECT_EQ(psMoveConstructor.get_buffer_source(), protocol_serializer::buffer_source::internal);
     EXPECT_NE(psMoveConstructor.get_internal_buffer().get(), nullptr);
@@ -63,12 +112,12 @@ TEST(Constructing, Constructors) {
     EXPECT_EQ(psMoveConstructor.get_internal_buffer_length(), 3);
     EXPECT_EQ(psMoveConstructor.get_fields_list().size(), 3);
     // Check if it is now empty
-    EXPECT_EQ(psConstructor2.get_is_little_endian(), true);
-    EXPECT_EQ(psConstructor2.get_buffer_source(), protocol_serializer::buffer_source::internal);
-    EXPECT_EQ(psConstructor2.get_internal_buffer().get(), nullptr);
-    EXPECT_EQ(psConstructor2.get_external_buffer(), nullptr);
-    EXPECT_EQ(psConstructor2.get_internal_buffer_length(), 0);
-    EXPECT_EQ(psConstructor2.get_fields_list().size(), 0);
+    EXPECT_EQ(ps.get_is_little_endian(), true);
+    EXPECT_EQ(ps.get_buffer_source(), protocol_serializer::buffer_source::internal);
+    EXPECT_EQ(ps.get_internal_buffer().get(), nullptr);
+    EXPECT_EQ(ps.get_external_buffer(), nullptr);
+    EXPECT_EQ(ps.get_internal_buffer_length(), 0);
+    EXPECT_EQ(ps.get_fields_list().size(), 0);
 
     // Move assignment operator
     protocol_serializer psMoveAssignment(std::move(psMoveConstructor));
@@ -87,17 +136,21 @@ TEST(Constructing, Constructors) {
     EXPECT_EQ(psMoveConstructor.get_fields_list().size(), 0);
 }
 
-TEST(Constructing, GettersAndSetters)
+TEST(Modifying, Endiannes)
 {
     protocol_serializer ps;
 
-    // Endiannes
     ps.set_is_little_endian(true);
     EXPECT_EQ(ps.get_is_little_endian(), true);
     ps.set_is_little_endian(false);
     EXPECT_EQ(ps.get_is_little_endian(), false);
     ps.set_is_little_endian(true);
     EXPECT_EQ(ps.get_is_little_endian(), true);
+}
+
+TEST(Modifying, ProtocolLayout)
+{
+    protocol_serializer ps;
 
     // Appending fields
     EXPECT_NE(ps.append_field({"field_1", 0}), result_code::ok);
